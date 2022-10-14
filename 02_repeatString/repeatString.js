1e1e7727ad380e758528f8be6b8d7d1bf411897c
Tilen Marš <marstilen@gmail.com>
2022-10-14 11:50:26 +0200

    Finish excersise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (repeatedPhase, numberOfRepeats) {
+  let outputString = "";
+  if (numberOfRepeats < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < numberOfRepeats; i++) {
+      outputString += repeatedPhase;
+    }
+    return outputString;
+  }
 };
 
 // Do not edit below this line
