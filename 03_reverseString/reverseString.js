04229dda65a813255a738f5edf00655e2b4bf1c2
Tilen Marš <marstilen@gmail.com>
2022-10-26 08:06:36 +0200

    Solve excersise 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (givenText) {
+  let reversedString = "";
+  for (let i = givenText.length - 1; i >= 0; i--) {
+    reversedString += givenText[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
