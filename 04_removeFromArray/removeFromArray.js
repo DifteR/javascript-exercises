2d47b9c67b831cc40054a0d8b17bf240724518b0
Tilen Marš <its0black0magic@gmail.com>
2022-10-30 14:29:23 +0000

    Solve #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,10 @@
 const removeFromArray = function(arr, del) {
-    
-    //del -= 1;
     let index;
-    for (let i = 1; i < arguments.length; i++) {
-        index = arr.indexOf(arguments[i]);
-        arr.splice(index, 1);  
+    for (let i = 1; i <= arguments.length; i++) {
+        if (arr.includes(arguments[i])) {
+            index = arr.indexOf(arguments[i]);
+            arr.splice(index, 1);  
+        }
     }
     return arr;
     };
