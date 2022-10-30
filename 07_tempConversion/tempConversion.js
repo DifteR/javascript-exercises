2b67a47e67a31fc1f83ef9cf39f888e6ae3e8799
Tilen Marš <its0black0magic@gmail.com>
2022-10-30 19:26:37 +0000

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+function roudNumber(decimalNumber) {
+  return Math.round(decimalNumber * 10) / 10;
+}
 
+const ftoc = function(tempatureF) {
+  let convertedTempature = (tempatureF-32)*(5/9);
+  convertedTempature = roudNumber(convertedTempature);
+  return convertedTempature;
 };
 
-const ctof = function() {
-
+const ctof = function(tempatureC) {
+  let convertedTempature = (tempatureC * 9/5 + 32)
+  convertedTempature = roudNumber(convertedTempature);
+  return convertedTempature;
 };
 
 // Do not edit below this line
