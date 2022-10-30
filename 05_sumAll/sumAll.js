25149342b1890a52d279073304f692bda98349dd
Tilen Marš <its0black0magic@gmail.com>
2022-10-30 18:39:06 +0000

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let total = 0; 
+    if(typeof min != 'number' || typeof max != 'number' || min < 0 || max < 0)
+    {
+        return 'ERROR';
+    }
+    else if (max<min) {
+        total= min;
+        min = max;
+        max = total;
+        total = 0;
+    }
+    while(min<=max)
+    {
+        total += min;
+        min++;
+    }
+   return total;
 };
 
 // Do not edit below this line
