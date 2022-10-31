9e83c73e73f0a599de56382cf2a28e5c36ae6a43
Tilen Marš <its0black0magic@gmail.com>
2022-10-31 08:02:00 +0000

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (givenText) {
+    let reversedString = "";
+    let normalisedString = "";
+    let alphaReg = new RegExp(/^[a-z]/i);
+    for (let i = givenText.length - 1; i >= 0; i--) {
+        if (alphaReg.test(givenText[i])) {
+            reversedString += givenText[i];
+        }
+    }
+    for (let i = 0; i < givenText.length; i++) {
+        if (alphaReg.test(givenText[i])) {
+            normalisedString += givenText[i];
+        }
+        
+    }
+    reversedString = reversedString.toLowerCase();
+    normalisedString = normalisedString.toLowerCase();
+    if (reversedString == normalisedString) {
+        return true;
+    }
+    else return false;
 
 };
 
