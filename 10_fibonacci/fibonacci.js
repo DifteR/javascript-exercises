c071367d96c0992541a887fe20aa853c599fcb42
Tilen Marš <its0black0magic@gmail.com>
2022-10-31 09:02:49 +0000

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+    parseInt(inputNum);
+    let i = 4;
+    if(inputNum > 0)
+    {
+        let sequence = [1, 1, 2, 3];
+            while(i <= inputNum){
+                sequence.push((sequence[i-1])+(sequence[i-2]));
+                i++;
+            }
+        return sequence[inputNum-1];
+    }
+    else return "OOPS";
 };
 
 // Do not edit below this line
