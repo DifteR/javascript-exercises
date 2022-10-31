674abaa332d831ffbd6084738094de5f86088a7e
Tilen Marš <its0black0magic@gmail.com>
2022-10-31 19:17:51 +0000

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+    let ageOfCurrentPerson;
+    let currentOldest = 0;
+    let nameOfOldest = "";
+    let currentYear = new Date().getFullYear();
+    for(i = 0; i < arrayOfPeople.length; i++){
+        if (!arrayOfPeople[i].yearOfDeath) {
+            ageOfCurrentPerson = currentYear - arrayOfPeople[i].yearOfBirth;
+        }
+        else{
+            ageOfCurrentPerson = arrayOfPeople[i].yearOfDeath - arrayOfPeople[i].yearOfBirth;
+        }
+        if (ageOfCurrentPerson > currentOldest) {
+            currentOldest = ageOfCurrentPerson;
+            nameOfOldest = arrayOfPeople[i]; 
+        }
+    }
+    return nameOfOldest;
 };
 
 // Do not edit below this line
