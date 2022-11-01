584a1a12ebde387f01d674b1e82e694c63d774e4
Tilen Marš <its0black0magic@gmail.com>
2022-11-01 11:20:21 +0000

    Backup for 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,19 @@
-const caesar = function() {
-
+const caesar = function(input, jump) {
+    let isANumberReg = new RegExp(/^[a-z]/i);
+    let currentChar;
+    let currentNuberUnicodeValue;
+    let encryptedText = "";
+    for (let i = 0; i < input.length; i++) {
+        currentNuberUnicodeValue = input.charCodeAt([i]);
+        if (isANumberReg) {
+            if (currentNuberUnicodeValue + jump > 91) {
+                currentNuberUnicodeValue += jump - 27;
+            }
+            else currentNuberUnicodeValue += jump;
+        }
+        encryptedText += String.fromCharCode(parseInt(currentNuberUnicodeValue));
+    };
+    return encryptedText;
 };
 
 // Do not edit below this line
